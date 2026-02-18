853b983235a44b40c1cdb40af1e50f6a9858901b
Gopinath Raveendran <gopinath@eam360.com>
2026-02-18 17:20:36 +0530

    Importing SwiftUIIntrospect

--- Sources/TarkaUI/Components/Input Field/Sub components/TUIInputTextContentView.swift
+++ Sources/TarkaUI/Components/Input Field/Sub components/TUIInputTextContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import Combine
+import SwiftUIIntrospect
 
 /// This is SwiftUI View that displays title and text content for the `TUIInputField` view in the vertical stack.
 /// The view can be customized with different styles,
